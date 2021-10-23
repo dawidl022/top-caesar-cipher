b3be32ab55088130fcb5678cdcf1b27256149eb9
dawidl022 <domdaw7@gmail.com>
2021-10-23 20:21:59 +0100

    Add method documentation

--- cipher.rb
+++ cipher.rb
@@ -1,12 +1,23 @@
+# Check if the given character is a unicode letter (alphabet symbol)
+# @param [String] char a string with a single character
+# @return [Boolean] true if the character is a letter, false otherwise
 def letter?(char)
   # solution from https://stackoverflow.com/a/14551970
   char.match?(/[[:alpha:]]/)
 end
 
+# Check if the given string is entirely in uppercase
+# @param [String] string to be checked
+# @return [Boolean] true if the string is uppercase, false otherwise
 def is_upper?(string)
   string == string.upcase
 end
 
+# Shift a letter in the ASCII alphabet by key places, wrapping around to the
+# start of the alphabet if necessary. Preserves case.
+# @param [String] letter an ASCII letter character
+# @param [Integer] key shift letter by key positions
+# @return [String] letter shifted by key positions in alphabet
 def shift(letter, key)
   alphabet_end_code = is_upper?(letter) ? "Z".ord : "z".ord
   letter_code = letter.ord
@@ -20,6 +31,11 @@ def shift(letter, key)
   ciphered_code.chr
 end
 
+# Cipher a string using the caesar cipher to shift all letters in the string
+# by a given key.
+# @param [String] text_to_cipher The input text to be ciphered
+# @param [Integer] key by how many positions to shift each letter in the text
+# @return [String] the ciphered text
 def caesar_cipher(text_to_cipher, key)
   # ensure key is a value between 0 and 25
   key = key % 26
